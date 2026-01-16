e1111eab098da33a7e0218e78e636747533ff4b0
Vokich <kolupaevvova638@gmail.com>
2026-01-16 19:16:42 +0300

    rework requirements

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -o errexit
-python -m gunicorn your_project.wsgi:application --workers 4 --bind 0.0.0.0:$PORT
+python -m gunicorn lipetsk.wsgi:application --workers 4 --bind 0.0.0.0:$PORT
